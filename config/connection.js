b4c8de156244206e83670fe28746606e1a97feff
Cristina Geremia <CG@Cristinas-Air.lan>
2023-04-25 22:17:27 -0400

    prompt user to choose from directory to  add/update/view dept and employees

--- config/connection.js
+++ config/connection.js
@@ -10,4 +10,9 @@ const connection = mysql.createConnection({
   database: "Employee_DB",
 });
 
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database')
+});
+
 module.exports = connection;
--- server.js
+++ server.js
@@ -20,10 +20,10 @@ const promptUser = () => {
                 'View All Departments',
                 'View All Employees',
                 'View All Roles',
-                'Update Employee Role',
                 'Add Employee',
                 'Add Role',
                 'Add Department',
+                'Update Employee Role',
                 'Exit'
             ]
         }
@@ -64,3 +64,137 @@ const promptUser = () => {
         }
     });
 };
+
+// View 
+const viewAllEmployees = () => {
+    connection.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    });
+};
+
+const viewAllDepartments = () => { 
+    connection.query('SELECT * FROM departments', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    });
+};
+
+const viewAllRoles = () => {
+    connection.query('SELECT * FROM roles', (err, res) => {
+        if (err) throw err;
+        console.tables(res);
+        promptUser();
+    });
+};
+
+// Add 
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            name: 'firstname',
+            type: 'input',
+            message: 'What is the employees first name?'
+        },
+        {
+            name: 'lastname',
+            type: 'input',
+            message: 'What is the employees last name?'
+        },
+        {
+            name: 'role',
+            type: 'input',
+            message: 'What is the employees role?'
+        },
+        {
+            name: 'id',
+            type: 'input',
+            message: 'What is the employees id?'
+        },
+        {
+            name: 'manager',
+            type: 'input',
+            message: 'Who is their manager?'
+        }
+    ])
+    .then((answer) => { 
+        connection.query('INSERT INTO employee SET ?', answer, (err) => {
+            if (err) throw err;
+            console.log('Employee added successfully!');
+            promptUser();
+        });
+    });
+};
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of this role?'
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'How much is the salary for this role?'
+        },
+        {
+            name: 'department',
+            type: 'input',
+            message: 'What is the department for this role?'
+        }
+    ])
+    .then((answer) => {
+        connection.query('INSERT INTO role SET ?', answer, (err) => { 
+            if (err) throw err;
+            console.log('Role added successfully!');
+            promptUser();
+        });
+    });
+};
+
+const addDepartment = () => { 
+    inquirer.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is the name of the department?'
+        }
+    ])
+    .then((answer) => { 
+        connection.query('INSERT INTO department SET ?', answer, (err) => {
+            if (err) throw err;
+            console.log('Department added successfully!');
+            promptUser();
+        });
+    });
+};
+
+// Update 
+
+const updateEmployeeRole = () => {
+    inquirer.prompt([
+        {
+            name: 'employee',
+            type: 'input',
+            message: 'What is the id of the employee?'
+        },
+        {
+            name: 'new_role',
+            type: 'input',
+            message: 'What is the new role for this employee?'
+        }
+    ])
+    .then((answer) => { 
+        connection.query('UPDATE employee SET role_id = ? WHERE id = ?', [answer.employee_id, answer.new_role], (err) => {
+            if (err) throw err;
+            console.log('Employee role updated successfully!');
+            promptUser();
+        });
+    });
+};
+
+promptUser();
